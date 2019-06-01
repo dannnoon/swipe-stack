ccc9484f44174703c6276931a44e646e457f04a2
Mariusz Wojtach <mariusz.wojtach@iteo.com>
2019-06-01 13:53:46 +0200

    wip oauth

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -6,7 +6,6 @@ import 'package:stack_matcher/api/Questions.dart';
 import 'package:http/http.dart' as http;
 
 
-
 var apiBaseUrl = "https://api.stackexchange.com";
 
 Future<Questions> getHttp() async {
@@ -21,4 +20,27 @@ Future<Questions> getHttp() async {
   } catch (e) {
     print(e);
   }
+}
+
+Future<String> authenticate(String code) async {
+  final dio = new Dio();
+  final response = await dio.post(
+    "https://stackoverflow.com/oauth/access_token/json",
+    data: {
+      "client_id": "15469",
+      "client_secret": "A2tApnThiDvQvu3o9e3E6A((",
+      "code": code,
+      "redirect_uri": "https://example.com",
+    },
+  );
+  return response.data.toString();
+}
+
+class OauthRequest {
+  final String client_id;
+  final String client_secret;
+  final String code;
+  final String redirect_uri;
+
+  OauthRequest(this.client_id, this.client_secret, this.code, this.redirect_uri);
 }
