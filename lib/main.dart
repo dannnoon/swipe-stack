8d2361573f5da8020cea12702f6be9ad5817b851
Denis Łaskawiec <denis.laskawiec@iteo.com>
2019-06-01 10:48:53 +0200

    Add gio

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:stack_matcher/api/endpoints.dart';
 import 'package:stack_matcher/home_page.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
